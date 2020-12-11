ebdb4c882a6b74aa46a36cf767bca97d7eb18b8c
Mareczek9191 <75856950+Mareczek9191@users.noreply.github.com>
2020-12-11 19:31:51 +0100

    Add files via upload

---
+++ esx_discord_bot/locales/en.lua
@@ -0,0 +1,37 @@
+Locales['en'] = {
+	['server'] = 'Serwer',
+	['server_start'] = 'Serwer Wystartowal',
+	['server_chat'] = 'Chat',
+
+	['server_connecting'] = 'Nowe po³¹czenie',
+	['user_connecting'] = '³¹czy siê',
+
+	['server_disconnecting'] = 'Nowe roz³¹czenie',
+	['user_disconnecting'] = 'roz³¹czy³ siê',
+
+	['server_item_transfer'] = 'Nowa transakcja (item)',
+	['server_money_transfer'] = 'Nowa transakcja (money)',
+	['server_moneybank_transfer'] = 'Nowa transakcja (moneybank)',
+	['server_weapon_transfer'] = 'Nowa transakcja (weapon)',
+
+	['user_gives_to'] = 'daje',
+
+	['server_washingmoney'] = 'Pranie pieniedzy Komunikat',
+	['user_washingmoney'] = 'wypral',
+
+	['server_blacklistedvehicle'] = 'BL samochod Alert',
+	['user_entered_in'] = 'Wsiadl do niego :',
+
+	['server_policecar'] = 'Police Alert',
+	['server_carjacking'] = 'Zajebanie radiowozu alert',
+	['user_carjacking'] = 'ukradl',
+
+	['server_kill'] = 'Smierci Alert',
+	['client_kill'] = 'zostal zabity przez',
+	['client_kill_environnement'] = 'jest martwy przez samobójstwo',
+	['with'] ='z',
+	
+
+	['user_kill'] = 'zostal zabity przez',
+	['user_kill_environnement'] = 'jest martwy przez samobójstwo',
+}
